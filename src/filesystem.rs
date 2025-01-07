8b299ef5aa819dbafcb8529299b56ea4ee7ec3c4
plant <alperensahin442@gmail.com>
2025-01-07 20:40:39 +0300

    test: add filesystem module tests

--- src/filesystem.rs
+++ src/filesystem.rs
@@ -40,3 +40,41 @@ pub fn get_words() -> Vec<&'static str> {
         .cloned()
         .collect()
 }
+
+pub fn file_exists(app_config_path: &PathBuf, file_name: &str) -> bool {
+  let absolute_path = app_config_path.join(file_name);
+  fs::metadata(&absolute_path).is_ok()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::constants::{TEST_APP_PATH, TEST_DB_NAME};
+
+    #[test]
+    fn test_file_exists() {
+        let mut path = PathBuf::new();
+        path.push(TEST_APP_PATH);
+
+        let correct_result = file_exists(&path, TEST_DB_NAME);
+        let wrong_result = file_exists(&path, "idk.db");
+
+        assert_eq!(correct_result, true);
+        assert_eq!(wrong_result, false);
+    }
+
+    #[test]
+    fn test_config_path() {
+        let app_config_path = get_app_config_path();
+        assert!(app_config_path.is_ok());
+    }
+
+    #[test]
+    fn test_return_ok_if_folder_exist() {
+        let mut path = PathBuf::new();
+        path.push(TEST_APP_PATH);
+
+        let result = create_config_folder(&path);
+        assert!(result.is_ok());
+    }
+}
